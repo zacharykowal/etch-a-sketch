b55541148aa0d6dad7330aa3c4a030f4bbb57512
Zachary Kowal <zacharykowal31@gmail.com>
2025-06-07 17:16:16 -0400

    Add grid resizing functionality

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const SIZE = 16;
+const DEFAULT_SIZE = 16;
 
 const gridContainer = document.querySelector(".container");
+const resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", resizeGrid);
 
-function createGrid() {
-    for (let i = 0; i < SIZE; i++) {
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         gridContainer.appendChild(row);
 
-        for (let i = 0; i < SIZE; i++) {
+        for (let i = 0; i < size; i++) {
             const box = document.createElement("div");
             box.classList.add("box");
             row.appendChild(box);
@@ -20,4 +22,25 @@ function createGrid() {
     }
 }
 
-createGrid();
+function removeGrid() {
+    gridContainer.replaceChildren();
+}
+
+function resizeGrid() {
+    let newSize;
+    let isValid = false;
+
+    while (!isValid) {
+        newSize = Number(prompt("Enter the new size (integer between 1 and 100):"));
+        if ((!isNaN(newSize)) && ((newSize >= 1) && (newSize <= 100)) && (Number.isInteger(newSize))) {
+            isValid = true;
+        } else {
+            alert("Invalid input. Try again.")
+        }
+    }
+
+    removeGrid();
+    createGrid(newSize);
+}
+
+createGrid(DEFAULT_SIZE);
